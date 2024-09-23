15133d94a9d9ebfbc6ffb7335f399b34ba4c8b20
Demyd <demidd06@gmail.com>
2024-09-23 20:11:39 +0300

    Third video with YouTube. I write code and fix problem with read path file.

--- javaSwingProject/src/entity/Player.java
+++ javaSwingProject/src/entity/Player.java
@@ -6,6 +6,7 @@ import main.KeyHandler;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 public class Player extends Entity {
@@ -27,33 +28,57 @@ public class Player extends Entity {
     }
     public void getPlayerImage(){
         try{
-            up1 = ImageIO.read(getClass().getResourceAsStream("res/player/boy_up_1.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("res/player/boy_up_2.png"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("res/player/boy_down_1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("res/player/boy_down_2.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("res/player/boy_left_1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("res/player/boy_left_2.png"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("res/player/boy_right_1.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("res/player/boy_right_2.png"));
+
+            up1 = ImageIO.read(new File("javaSwingProject\\res\\player\\boy_up_1.png"));
+            up2 = ImageIO.read(new File("javaSwingProject\\res\\player\\boy_up_2.png"));
+            down1 = ImageIO.read(new File("javaSwingProject\\res\\player\\boy_down_1.png"));
+            down2 = ImageIO.read(new File("javaSwingProject\\res\\player\\boy_down_2.png"));
+            left1 = ImageIO.read(new File("javaSwingProject\\res\\player\\boy_left_1.png"));
+            left2 = ImageIO.read(new File("javaSwingProject\\res\\player\\boy_left_2.png"));
+            right1 = ImageIO.read(new File("javaSwingProject\\res\\player\\boy_right_1.png"));
+            right2 = ImageIO.read(new File("javaSwingProject\\res\\player\\boy_right_2.png"));
+
+            /* ---------------------------------------Code--Don't--Work-------------------------------------------------------
+            |    up1 = ImageIO.read(getClass().getResourceAsStream("javaSwingProject\\res\\player\\boy_up_1.png"));           |
+            |    up2 = ImageIO.read(getClass().getResourceAsStream("javaSwingProject\\res\\player\\boy_up_2.png"));           |
+            |    down1 = ImageIO.read(getClass().getResourceAsStream("javaSwingProject\\res\\player\\boy_down_1.png"));       |
+            |    down2 = ImageIO.read(getClass().getResourceAsStream("javaSwingProject\\res\\player\\boy_down_2.png"));       |
+            |    left1 = ImageIO.read(getClass().getResourceAsStream("javaSwingProject\\res\\player\\boy_left_1.png"));       |
+            |    left2 = ImageIO.read(getClass().getResourceAsStream("javaSwingProject\\res\\player\\boy_left_2.png"));       |
+            |    right1 = ImageIO.read(getClass().getResourceAsStream("javaSwingProject\\res\\player\\boy_right_1.png"));     |
+            |    right2 = ImageIO.read(getClass().getResourceAsStream("javaSwingProject\\res\\player\\boy_right_2.png"));     |
+            ---------------------------------------------------------------------------------------------------------------- */
         }catch (IOException e){
             e.printStackTrace();
         }
     }
 
     public void update(){
-        if(keyH.upPressed == true){
-            direction = "up";
-            y -= speed;
-        }else if(keyH.downPressed == true){
-            direction = "down";
-            y += speed;
-        }else if(keyH.leftPressed == true){
-            direction = "left";
-            x -= speed;
-        }else if(keyH.rightPressed == true){
-            direction = "right";
-            x += speed;
+        if (keyH.downPressed == true || keyH.upPressed == true || keyH.leftPressed == true || keyH.rightPressed == true){
+            if(keyH.upPressed == true){
+                direction = "up";
+                y -= speed;
+            }else if(keyH.downPressed == true){
+                direction = "down";
+                y += speed;
+            }else if(keyH.leftPressed == true){
+                direction = "left";
+                x -= speed;
+            }else if(keyH.rightPressed == true){
+                direction = "right";
+                x += speed;
+            }
+            spriteCounter++;
+            if(spriteCounter > 12){
+                if (spriteNum == 1){
+                    spriteNum = 2;
+                }else if (spriteNum == 2){
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
         }
+
     }
     public void draw(Graphics2D g2){
 //        g2.setColor(Color.WHITE);
@@ -62,16 +87,36 @@ public class Player extends Entity {
         BufferedImage image = null;
         switch (direction){
             case "up":
-                image = up1;
+                if (spriteNum == 1){
+                    image = up1;
+                }
+                if (spriteNum == 2){
+                    image = up2;
+                }
                 break;
             case "down":
-                image = down1;
+                if (spriteNum == 1){
+                    image = down1;
+                }
+                if (spriteNum == 2){
+                    image = down2;
+                }
                 break;
             case "left":
-                image = left1;
+                if (spriteNum == 1){
+                    image = left1;
+                }
+                if (spriteNum == 2){
+                    image = left2;
+                }
                 break;
             case "right":
-                image = right1;
+                if (spriteNum == 1){
+                    image = right1;
+                }
+                if (spriteNum == 2){
+                    image = right2;
+                }
                 break;
         }
         g2.drawImage(image, x,y,gp.tileSize,gp.tileSize, null);
