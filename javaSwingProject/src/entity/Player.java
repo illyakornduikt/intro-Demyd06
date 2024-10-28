9f8323ac8696f2009b4f10841da470878cb4c587
Demyd <demidd06@gmail.com>
2024-10-28 22:35:54 +0200

    8 video with YouTube.

--- javaSwingProject/src/entity/Player.java
+++ javaSwingProject/src/entity/Player.java
@@ -15,6 +15,7 @@ public class Player extends Entity {
 
     public final int screenX;
     public final int screenY;
+    int hasKey = 0;
 
     public Player(GamePanel gp, KeyHandler keyH){
         this.gp = gp;
@@ -26,6 +27,8 @@ public class Player extends Entity {
         solidArea = new Rectangle();
         solidArea.x = 8;
         solidArea.y = 16;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
         solidArea.width = 32;
         solidArea.height = 32;
 
@@ -80,6 +83,9 @@ public class Player extends Entity {
             //CHECK TILE COLLISION
             collisionOn = false;
             gp.cChecker.checkTile(this);
+            // CHECK OBJECT COLLISION
+            int objIndex  = gp.cChecker.checkObject(this,true);
+            pickUpObject(objIndex);
             // IF COLLISION FALSE, PLAYER CAN MOVE
             if(collisionOn == false){
                 switch (direction){
@@ -109,6 +115,25 @@ public class Player extends Entity {
             }
         }
 
+    }
+    public void pickUpObject(int i){
+        if(i != 999){
+           String objectName = gp.obj[i].name;
+           switch (objectName){
+               case "Key":
+                   hasKey++;
+                   gp.obj[i] = null;
+                   System.out.println("Key:" + hasKey);
+                   break;
+               case "Door":
+                   if(hasKey > 0){
+                       gp.obj[i] = null;
+                       hasKey--;
+                   }
+                   System.out.println("Key:" + hasKey);
+                   break;
+           }
+        }
     }
     public void draw(Graphics2D g2){
 //        g2.setColor(Color.WHITE);
